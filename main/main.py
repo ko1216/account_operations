3b0fc438c19c3d7c7cadc649d89d928f153e5a20
korchaginks <kkonstantinserg@gmail.com>
2023-06-04 23:36:49 +0300

    -изменил main с учетом сортировки по дате

--- main/main.py
+++ main/main.py
@@ -1,7 +1,6 @@
-from utils.funcs import load_operations, get_latest_executed_operations, get_operations_details
+from utils.funcs import load_operations, get_latest_executed_operations, get_operations_details, data_sorted_operations
 
-
-latest_executed_operations = get_latest_executed_operations(load_operations())
+latest_executed_operations = get_latest_executed_operations(data_sorted_operations(load_operations()))
 
 for operation in latest_executed_operations:
     print(get_operations_details(operation))
